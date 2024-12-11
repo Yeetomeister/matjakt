c75832a91aa7f090fd01033ea0ffe7b248d4a47f
Trym Jakobsen <trjak5231@uib.no>
2024-12-11 18:59:12 +0100

    Add API test route

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,15 +1,21 @@
+// src/app/api/test/route.ts
+
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export async function GET() {
-    const newProduct = await prisma.product.create({
+    // Opprett et nytt produkt uten å tildele det til en variabel
+    await prisma.product.create({
         data: {
             name: "Test Product"
         }
     });
 
+    // Hent ut alle produkter
     const products = await prisma.product.findMany();
+
+    // Returner produktene i responsen
     return NextResponse.json(products);
 }
